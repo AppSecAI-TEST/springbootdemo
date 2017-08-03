e45155a21a51ef01bc81b03c3ded640b40f88545
Mr.Zhao <openedr@qq.com>
2017-08-03 14:41:49 +0800

    springboot demo3

--- src/main/java/com/demo/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/demo/springbootdemo/SpringbootdemoApplication.java
@@ -2,11 +2,36 @@ package com.demo.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
 
+import com.demo.springbootdemo.servlet.MyServlet;
+//主Application
 @SpringBootApplication
+//启用servlet 注解
+@ServletComponentScan   
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
+	
+    /**
+     * 使用代码注册Servlet（不需要@ServletComponentScan注解）
+     * "/xs/*" servlet 请求匹配
+     * 
+     * 修改DispatcherServlet默认配置
+     * 
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
+//        return new ServletRegistrationBean(new MyServlet(), "/xs/*");// ServletName默认值为首字母小写，即myServlet
+        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
+        registration.getUrlMappings().clear();
+        registration.addUrlMappings("*.do");
+        registration.addUrlMappings("*.json");
+        return registration;
+    }
 }
